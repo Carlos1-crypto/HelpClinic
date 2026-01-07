10b9f74af9661ff492f30e317299d860e3d62ec7
Carlos <chspv2007@gmail.com>
2026-01-07 09:31:43 -0300

    Otimização de if's
    Substituição de if/elif/else com match e case

--- main.py
+++ main.py
@@ -59,108 +59,89 @@ def recepcao():
             print(msg + pontos)
             time.sleep(0.5)
 
-        if resp == '1':
-            cadastro()
+        match resp:
+            case '1':
+                cadastro()
 
-        elif resp == '2':
-            print('\n=== Informações do Sistema ===\n')
+            case '2':
+                print('\n=== Informações do Sistema ===\n')
 
-            if not pacientes:
-                print('Nenhum cliente cadastrado.')
-                continue
+                if not pacientes:
+                    print('Nenhum cliente cadastrado.')
+                    continue
 
-            total_anos = 0
-            for paciente in pacientes:
-                total_anos += paciente.idade
-            media = total_anos / len(pacientes)
-            print(f'A idade média dos pacientes é de {media} anos.')
+                total_anos = 0
+                for paciente in pacientes:
+                    total_anos += paciente.idade
+                media = total_anos / len(pacientes)
+                print(f'A idade média dos pacientes é de {media} anos.')
 
-            print(f"O sistema possui um total de {len(pacientes)} pacientes.")
+                print(f"O sistema possui um total de {len(pacientes)} pacientes.")
 
-            mais_novo = pacientes[0]
-            pac_mais_novos = []
-            for pac in pacientes:
-                if pac.idade < mais_novo.idade:
-                    mais_novo = pac
-            for pac in pacientes:
-                if pac.idade == mais_novo.idade:
-                    pac_mais_novos.append(pac)
-            if len(pac_mais_novos) == 1:
-                print(f'Dentre eles, o(a) paciente mais novo(a) é o(a) {mais_novo.nome} com {mais_novo.idade} anos!')
-            elif len(pac_mais_novos) > 1:
-                print(f'Dentre eles temos os nossos pacientes mais novos com {mais_novo.idade} anos! Estes são:')
+                mais_novo = pacientes[0]
+                pac_mais_novos = []
+                for pac in pacientes:
+                    if pac.idade < mais_novo.idade:
+                        mais_novo = pac
+                for pac in pacientes:
+                    if pac.idade == mais_novo.idade:
+                        pac_mais_novos.append(pac)
+                if len(pac_mais_novos) == 1:
+                    print(f'Dentre eles, o(a) paciente mais novo(a) é o(a) {mais_novo.nome} com {mais_novo.idade} anos!')
+                elif len(pac_mais_novos) > 1:
+                    print(f'Dentre eles temos os nossos pacientes mais novos com {mais_novo.idade} anos! Estes são:')
+                    num = 1
+                    for pac in pac_mais_novos:
+                        print(f'{num}. {pac.nome}')
+                        num = num + 1
+
+                mais_velho = pacientes[0]
+                pac_mais_velhos = []
+                for pac in pacientes:
+                    if pac.idade > mais_velho.idade:
+                        mais_velho = pac
+                for pac in pacientes:
+                    if pac.idade == mais_velho.idade:
+                        pac_mais_velhos.append(pac)
+                if len(pac_mais_velhos) == 1:
+                    print(f'Dentre eles, o(a) paciente mais velho(a) é o {mais_velho.nome} com {mais_velho.idade} anos!')
+                elif len(pac_mais_velhos) > 1:
+                    print(f'Dentre eles temos os nossos pacientes mais velhos com {mais_velho.idade} anos! Estes são:')
+                    num = 1
+                    for pac in pac_mais_velhos:
+                        print(f'{num}. {pac.nome}')
+                        num = num + 1
+
+            case '3':
+                print('\n=== Busca de Paciente ===\n')
+                procura = input('Qual é o nome do paciente que está procurando?\n'
+                                'Paciente: ')
+                encontrado = None
+
+                for paciente in pacientes:
+                    if procura.lower() in paciente.nome.lower():
+                        encontrado = paciente
+                if encontrado:
+                    print('\nAqui estão as informações do paciente:\n'
+                          f'Nome: {encontrado.nome}\n'
+                          f'Idade: {encontrado.idade}\n'
+                          f'Telefone: {encontrado.telefone}\n')
+                else:
+                    print(
+                        '\nPaciente não encontrado, verifique se o nome foi escrito corretamente ou cadastre o paciente primeiro.\n')
+
+            case '4':
+                print('\n=== Lista de Pacientes ===\n')
                 num = 1
-                for pac in pac_mais_novos:
-                    print(f'{num}. {pac.nome}')
+                for paciente in pacientes:
+                    print(f'{num}. {paciente.nome}')
                     num = num + 1
 
-            mais_velho = pacientes[0]
-            pac_mais_velhos = []
-            for pac in pacientes:
-                if pac.idade > mais_velho.idade:
-                    mais_velho = pac
-            for pac in pacientes:
-                if pac.idade == mais_velho.idade:
-                    pac_mais_velhos.append(pac)
-            if len(pac_mais_velhos) == 1:
-                print(f'Dentre eles, o(a) paciente mais velho(a) é o {mais_velho.nome} com {mais_velho.idade} anos!')
-            elif len(pac_mais_velhos) > 1:
-                print(f'Dentre eles temos os nossos pacientes mais velhos com {mais_velho.idade} anos! Estes são:')
-                num = 1
-                for pac in pac_mais_velhos:
-                    print(f'{num}. {pac.nome}')
-                    num = num + 1
+            case '5':
+                print('Saindo...')
+                break
 
-        elif resp == '3':
-            print('\n=== Busca de Paciente ===\n')
-            procura = input('Qual é o nome do paciente que está procurando?\n'
-                            'Paciente: ')
-            encontrado = None
+            case _:
+                print("Opção inválida!")
 
-            for paciente in pacientes:
-                if procura.lower() in paciente.nome.lower():
-                    encontrado = paciente
-            if encontrado:
-                print('\nAqui estão as informações do paciente:\n'
-                      f'Nome: {encontrado.nome}\n'
-                      f'Idade: {encontrado.idade}\n'
-                      f'Telefone: {encontrado.telefone}\n')
-            else:
-                print(
-                    '\nPaciente não encontrado, verifique se o nome foi escrito corretamente ou cadastre o paciente primeiro.\n')
-
-        elif resp == '4':
-            print('\n=== Lista de Pacientes ===\n')
-            num = 1
-            for paciente in pacientes:
-                print(f'{num}. {paciente.nome}')
-                num = num + 1
-
-        elif resp == '5':
-            print('Saindo...')
-            break
-
-        else:
-            print("Opção inválida!")
-
-
-def loader_animado():
-    caracters = ['◟', '◜', '◝', '◞']
-    total = 30
-    contador = 0
-    while contador < total:
-        for i in range(30):
-            for char in caracters:
-                if contador == total:
-                    break
-
-                contador += 1
-                texto = f'{char} {contador}/{total}'
-                sys.stdout.write(f'\r + {texto}')
-                sys.stdout.flush()
-                time.sleep(0.05)
-        sys.stdout.write(f'\r' + '')
-        print('Concluído com sucesso!\n')
-
-loader_animado()
 recepcao()
